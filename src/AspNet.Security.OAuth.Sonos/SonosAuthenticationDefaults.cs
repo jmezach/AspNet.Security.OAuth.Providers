c89880ac343c7a957c21c3340924629e04db05c9
Jonathan Mezach <jonathanm@infosupport.com>
2018-09-17 20:19:13 +0200

    Added a Sonos OAuth provider

---
+++ src/AspNet.Security.OAuth.Sonos/SonosAuthenticationDefaults.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
+
+namespace AspNet.Security.OAuth.Sonos
+{
+    /// <summary>
+    /// Default values used by the Sonos authentication middleware.
+    /// </summary>
+    public static class SonosAuthenticationDefaults
+    {
+        /// <summary>
+        /// Default value for <see cref="AuthenticationScheme.Name"/>.
+        /// </summary>
+        public const string AuthenticationScheme = "Sonos";
+
+        /// <summary>
+        /// Default value for <see cref="AuthenticationScheme.DisplayName"/>.
+        /// </summary>
+        public const string DisplayName = "Sonos";
+
+        /// <summary>
+        /// Default value for <see cref="AuthenticationSchemeOptions.ClaimsIssuer"/>.
+        /// </summary>
+        public const string Issuer = "Sonos";
+
+        /// <summary>
+        /// Default value for <see cref="RemoteAuthenticationOptions.CallbackPath"/>.
+        /// </summary>
+        public const string CallbackPath = "/signin-sonos";
+
+        /// <summary>
+        /// Default value for <see cref="OAuthOptions.AuthorizationEndpoint"/>.
+        /// </summary>
+        public const string AuthorizationEndpoint = "https://api.sonos.com/login/v3/oauth";
+
+        /// <summary>
+        /// Default value for <see cref="OAuthOptions.TokenEndpoint"/>.
+        /// </summary>
+        public const string TokenEndpoint = "https://api.sonos.com/login/v3/oauth/access";
+
+        /// <summary>
+        /// Default value for <see cref="OAuthOptions.UserInformationEndpoint"/>.
+        /// </summary>
+        public const string UserInformationEndpoint = "https://api.ws.sonos.com/control/api/v1/households";
+    }
+}
---
+++ src/AspNet.Security.OAuth.Sonos/SonosAuthenticationOptions.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json.Linq;
+using static AspNet.Security.OAuth.Sonos.SonosAuthenticationConstants;
+
+namespace AspNet.Security.OAuth.Sonos
+{
+    /// <summary>
+    /// Defines a set of options used by <see cref="SonosAuthenticationHandler"/>.
+    /// </summary>
+    public class SonosAuthenticationOptions : OAuthOptions
+    {
+        public SonosAuthenticationOptions()
+        {
+            ClaimsIssuer = SonosAuthenticationDefaults.Issuer;
+            Scope.Add("playback-control-all");
+
+            CallbackPath = new PathString(SonosAuthenticationDefaults.CallbackPath);
+
+            AuthorizationEndpoint = SonosAuthenticationDefaults.AuthorizationEndpoint;
+            TokenEndpoint = SonosAuthenticationDefaults.TokenEndpoint;
+            UserInformationEndpoint = SonosAuthenticationDefaults.UserInformationEndpoint;
+
+            ClaimActions.MapCustomJson(ClaimTypes.NameIdentifier, payload => payload.Value<JArray>("households")?.First?.Value<string>("id"));
+        }
+    }
+}
